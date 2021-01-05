c02ea6bca5f953c17d14000d1fbc975a06dfae00
Yury Samarychau <qw4n7y@gmail.com>
2021-01-06 00:46:03 +0300

    initial

---
+++ static/js/6.1c1630da.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpsalt-and-satoshi"]=this["webpackJsonpsalt-and-satoshi"]||[]).push([[6],{840:function(s,a){}}]);
+//# sourceMappingURL=6.1c1630da.chunk.js.map
--- static/js/6.e5da2c83.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpsalt-and-satoshi"]=this["webpackJsonpsalt-and-satoshi"]||[]).push([[6],{816:function(s,a){}}]);
-//# sourceMappingURL=6.e5da2c83.chunk.js.map
